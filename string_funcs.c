b9e932052ea664983eaeac87ab15777dcdd190ad
Rahul Madhavan <rahul.maddy@gmail.com>
2018-12-17 01:09:59 +0530

    atoi

--- string_funcs.c
+++ string_funcs.c
@@ -30,6 +30,35 @@ void stringreverse(char* s){
     j = stringlen(s)-1;
     while(i<j)swap_characters(s,i++,j--);
 }
+char* inttos(int n, char s[]){
+    int i, temp;
+    (n<0)?temp=-n:(temp = n);
+    
+    s[0] = temp%10 +'0';
+    temp/=10;
+    for(i=1;temp!=0;i++){
+        s[i] = temp%10 +'0';
+        temp/=10;
+    }
+    // a nearly unreadable for loop doing the above in 1 line
+    // for(s[0] = temp%10 +'0',temp/=10,i=1;temp!=0;s[i++] = temp%10 +'0',temp/=10);
+
+    if(n<0)(s[i++] = '-');
+    s[i]='\0';
+    stringreverse(s);
+    return s;
+}
+int ascii_to_int(char s[]){
+    int i,n,start_i;
+    n=0;
+    s[0]=='-'?(start_i = 1):(start_i = 0);
+    for(i=start_i;s[i]>='0'&&s[i]<='9';i++){
+        n *=10;
+        n+= (s[i]-'0');
+    }
+    if(start_i==1)n*=-1;
+    return n;
+}
 
 void main(void){
     char a[] = "abce";
@@ -45,4 +74,14 @@ void main(void){
     printf("reversed string d is %s\n", d);
     stringreverse(d);
     printf("reversed reversed string d is %s\n", d);
+    int n = 123;
+    char e[5];
+    printf("123 as string is %s\n",inttos(n,e));
+    printf("-123 as string is %s\n",inttos(-n,e));
+    printf("0 as string is %s\n",inttos(0,e));
+    char f[4] = "123";
+    printf("the number 123 converted to int is %d\n",ascii_to_int(f));
+    // you can't simply assign to existing arrays
+    stringcpy("-123",f);
+    printf("the number -123 converted to int is %d\n",ascii_to_int(f));
 }
